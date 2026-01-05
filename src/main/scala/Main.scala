80cfd7736618eab8c89c19acd554d87feb3abfa5
jennifer <cxxyao2@gmail.com>
2026-01-04 22:31:43 -0500

    add Column, DataFrame

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,8 +1,7 @@
 package de.xixi.sparkcourse
 
-import org.apache.spark.sql.SparkSession
-
-
+import org.apache.spark.sql.functions.col
+import org.apache.spark.sql.{DataFrame, SparkSession}
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 object Main {
   def main(args: Array[String]): Unit = {
@@ -12,13 +11,21 @@ object Main {
       .config("spark.driver.bindAddress", "127.0.0.1")
       .getOrCreate()
 
-    val df = spark.read
+    val df: DataFrame = spark.read
       .option("header", value = true)
       .option("inferSchema", value = true)
       .csv("data/AAPL.csv")
 
     df.show()
     df.printSchema()
+
+    df.select("Date","Open","Close").show()
+    val column = df("Date")
+    col("Date")
+    import spark.implicits._
+    $"Date"
+
+    df.select(col("Date"), $"Open", df("Close")).show()
 
   }
 }
